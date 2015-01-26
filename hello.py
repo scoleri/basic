ee815a6d0f4323246217dd53c1d9f22172af302d
scoleri <scoleri@gmail.com>
2015-01-26 11:46:50 -0500

    downloaded class shit

--- hello.py
+++ hello.py
@@ -1,9 +1,17 @@
-#!/usr/bin/python2.4 -tt
+#!/usr/bin/python2.4
 
 import sys
 
+def Hello(name):
+	if name == 'Alice' or name == 'Nick':
+		name = name + '????'
+	else:
+		print 'Else'
+	name=name+'!!!!!'
+	print "Hello",name
+
 def main():
-	print "Hello"
+	Hello(sys.argv[1])
 
 # This is the standard boilerplate that calls the main() function.
 if __name__ == '__main__':
