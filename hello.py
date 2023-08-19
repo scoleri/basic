90cc98e0164e55297e3aa9eac1ebe5f21de94c42
Steven Scoleri <scoleri@gmail.com>
2023-08-19 07:53:45 -0400

    playing with copilot

--- hello.py
+++ hello.py
@@ -3,6 +3,11 @@
 import sys
 
 def Hello(name):
+	"""
+	This function takes a name as input and prints a greeting message with the name.
+	If the name is 'Alice' or 'Nick', it adds '????' to the name before printing the message.
+	If the name is anything else, it adds '!!!!!' to the name before printing the message.
+	"""
 	if name == 'Alice' or name == 'Nick':
 		name = name + '????'
 	else:
@@ -11,9 +16,11 @@ def Hello(name):
 	print "Hello",name
 
 def main():
+	"""
+	This function calls the Hello function with the first command line argument passed to the script.
+	"""
 	Hello(sys.argv[1])
 
 # This is the standard boilerplate that calls the main() function.
 if __name__ == '__main__':
 	main()
-
